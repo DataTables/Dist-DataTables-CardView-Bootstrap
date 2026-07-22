c28f7f97e0c8bafd23b1f5e7ecf5fc48822d7dc1
dtbuild <dtbuild@rigil>
2026-07-22 12:55:36 +0000

    1321869da9b93343fb1fcc2bfb194c51b091cc96 Release 1.0.0!
    Sync to source repo @1321869da9b93343fb1fcc2bfb194c51b091cc96

--- js/cardView.bootstrap.min.mjs
+++ js/cardView.bootstrap.min.mjs
@@ -1,4 +1,4 @@
-/*! CardView Bootstrap 3 styling 1.0.0-dev for DataTables
+/*! CardView Bootstrap 3 styling 1.0.0 for DataTables
  * Copyright (c) SpryMedia Ltd - https://datatables.net/license/plus
  */
 import DataTable from"datatables.net-bs";import CardView from"datatables.net-cardview";Object.assign(DataTable.CardView.classes,{card:"panel panel-default dtcv-card",cardContent:"panel-body"});export default DataTable;
